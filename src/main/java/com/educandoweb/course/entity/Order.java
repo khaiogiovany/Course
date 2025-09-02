8397011caebd585f310eada971f2c2203307bd1e
khaiogiovany <khaio_giovany@hotmail.com>
2025-09-02 10:52:21 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entity/Order.java
+++ src/main/java/com/educandoweb/course/entity/Order.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 @Table(name = "tb_order")
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
+    
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
